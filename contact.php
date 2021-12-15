7ffac142b659f88dfd2704cb3173ac59c9ed5647
Akshat Jain <akshatjain589@gmail.com>
2021-12-15 11:51:33 +0530

    Created first draft of Contact page
    Made required folders and also the contact page with responsiveness

---
+++ contact.php
@@ -0,0 +1,20 @@
+<?php $title="Contact Us"; include "header.php"; ?>
+<link rel="stylesheet" type="text/css" href="css/contact.css" />
+
+<div class="section1">
+   <div class="section2">
+        <input type="text" placeholder="Enter your Name">
+        <br>
+        <input type="text" placeholder="Enter Your Email">
+        <br>    
+        <textarea placeholder="Enter Your Message"></textarea>
+   </div>
+   <div class="section3">
+       <div class="contact_info">Contact</div>
+       <div class="contact_info"> Email</div>
+       <div class="contact_info"> Address</div>
+   </div>
+</div>
+    
+
+<?php include "footer.php";?>
--- header.php
+++ header.php
@@ -1,13 +1,97 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php if($title===""){$title='Default';}?>
     <title><?php if(isset($title)){echo $title;}?> | Ham Radio Club &mdash; IIT Bombay</title>
+    <link rel="stylesheet" type="text/css" href="css/header.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 </head>
+
 <body>
-    
-</body>
-</html>
+    <header>
+        <section class="navigation">
+            <div class="nav-container">
+                <div class="brand">
+                    <a href="index.php">
+                        <img src="images/hrc_logo.jpeg" id="im01">
+                    </a>
+                </div>
+                <nav>
+                    <div class="nav-mobile"><a id="nav-toggle" href="#!"><span></span></a></div>
+                    <ul class="nav-list">
+                        <li>
+                            <a href="#!">About us</a>
+                            <!-- <ul class="nav-dropdown">
+                    <li>
+                      <a href="#!">Intro</a>
+                    </li>
+                    <li>
+                      <a href="#!">Our radio shack</a>
+                    </li>
+                  </ul> -->
+                        </li>
+                        <li>
+                            <a href="#!">What we do</a>
+                            <ul class="nav-dropdown">
+                                <li>
+                                    <a href="#!">GSW (Flagship event)</a>
+                                </li>
+                                <li>
+                                    <a href="#!">Events</a>
+                                </li>
+                                <li>
+                                    <a href="#!">Tasks</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#!">Blog</a>
+                            <!-- <ul class="nav-dropdown">
+                  <li>
+                    <a href="#!">Stones of a Naive Radio</a>
+                  </li>
+                </ul> -->
+                        </li>
+                        <li>
+                            <a href="#!">Ham Exam</a>
+                            <!-- <ul class="nav-dropdown">
+                    <li>
+                      <a href="#!">Process</a>
+                    </li>
+                  </ul> -->
+                        </li>
+                        <li>
+                            <a href="#!">Team</a>
+                            <!-- <ul class="nav-dropdown">
+                  <li>
+                    <a href="#!">HRC Members</a>
+                  </li>
+                  <li>
+                    <a href="#!">Past Managers</a>
+                  </li>
+                </ul> -->
+                        </li>
+                        <li>
+                            <a href="#!">Media</a>
+                            <!-- <ul class="nav-dropdown">
+                    <li>
+                      <a href="#!">Featured articles</a>
+                    </li>
+                  </ul> -->
+                        </li>
+                        <li>
+                            <a href="contact.php">Contact us</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </section>
+        <div class="page_header">
+        <img src="images/hrc_logo.jpeg" alt="Logo" class="page_logo">
+        <div class="page_heading"><?php echo($title) ?></div>
+        </div>
+    </header>
+    <script src="js/header.js"></script>
