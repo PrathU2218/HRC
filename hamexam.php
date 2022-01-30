7f039afd844ea1b1618ebcfcc8d30d0467f663f5
raghavrander <81479687+raghavrander@users.noreply.github.com>
2022-01-30 23:53:06 +0530

    All changes in hamexam page done

--- hamexam.php
+++ hamexam.php
@@ -9,65 +9,59 @@ include 'header.php'; ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
     </script>
+    <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
     
 </head>
 <div class="container-fluid" id="i1">
-   <div class="row">
-       <div class="col-xs" id="text3">
-           You cannot enjoy all the benefits of ham radio without having a<br>Ham Radio License. You can only receive signals but you need to<br>have a licence and a callsign for legally transmitting on ham radio.<br> Once you have the ham radio license, you can talk to other hams,<br> build your own ground station, participate in contests and foxhunts<br> and also provide communication assistance during emergencies. 
-       </div>
-       <div class="col-xs" id="text4">
-           There are two<br>categories of<br>licenses
-       </div>
-       <div style="float:right;margin-top:10px"><i class="fa fa-long-arrow-down" style="font-size:36px;float:right"></i></div>
-   </div>
-   <div id="text5">
-            RESTRICTED GRADE<br>
-            permitting the use of medium power (10 W on VHF and UHF, 50 W on HF)<br><br>
-
-            GENERAL GRADE<br>
-            permitting the use of higher power (25 W on VHF and UHF, 400 W on HF)<br> 
+   <div class="row" id="left">
+       <div class="col-sm-6" id="text3">
+          You cannot enjoy all the benefits of ham radio without having a Ham Radio License. You can only receive signals but you need to have a license and a callsign for legally transmitting on ham radio. Once you have the ham radio license, you can talk to other hams, build your own ground station, participate in contests and foxhunts and also provide communication assistance during emergencies. 
+     </div>
+       <div class="col-sm-6" id="text4">
+          Amateur Station Operator's Certificate or ASOC is the examination that needs to be passed to receive an amateur radio license in India. The exam is conducted by the <a href="https://en.wikipedia.org/wiki/Wireless_and_Planning_and_Coordination_Wing" target="_blank">Wireless Planning and Coordination Wing</a> (WPC) of the <a href="https://en.wikipedia.org/wiki/Ministry_of_Communications_and_Information_Technology_(India)" target="_blank">Ministry of Communications and Information Technology</a>.
+        </div>   
   </div>
-  <!-- <div class="row">
-     <div class="col-xs" id="text6">
-         The examination consists of a 100 marks question<br> paper (50 marks related to basic radio/electronics<br> theory as per the syllabus + 50 marks related to<br> Amateur Radio Rules & Regulations) which is of one,<br> two, or three hours duration as per the “Grade” of<br> license you will be going for. For getting the General<br> Grade Licence, you also need to clear a practical test<br> on Morse Code sending and receiving. This is a 10<br> minutes duration test to examine the candidate's<br> proficiency in sending and receiving Morse Code.
+   <div class="row" id="left">
+       <div class="col-sm-6">
+           <div id="text5">There are two<br>categories<i class='fas fa-arrow-right' style="font-size:36px;color:black;margin-left:50px"></i><br>of licenses</div>
+           <div id="text6">RESTRICTED GRADE:<br>permitting the use of medium power(10 W on VHF and UHF, 50 W on HF)<br><br>
+           GENERAL GRADE:<br>permitting the use of higher power(25 W on VHF and UHF, 400 W on HF)</div>
+      </div>
+      <div class="col-sm-6" id="text7">
+          The examination consists of a 100 marks question paper (50 marks related to basic radio/electronics theory as per the syllabus + 50 marks related to Amateur Radio Rules & Regulations) which is of one, two, or three hours duration as per the “Grade” of license you will be going for. For getting the General Grade Licence, you also need to clear a practical test on Morse Code sending and receiving. This is a 10 minutes duration test to examine the candidate's proficiency in sending and receiving Morse Code.
      </div>
-     <div class="col-xs" id="text7">
-         Check out our Instagram series, ‘Morse Mania’, where we have posted the graphical representation of morse code for every alphabet and digit which can assist in remembering the morse codes visually. 
-     </div>
- </div>   -->
-  <div class="container-fluid">
-       <div class="row">
-          <div class="col-sm" id="text6">
-                The examination consists of a 100 marks question paper (50 marks related to basic radio/electronics theory as per the syllabus + 50 marks related to Amateur Radio Rules & Regulations) which is of one, two, or three hours duration as per the “Grade” of license you will be going for. For getting the General Grade Licence, you also need to clear a practical test on Morse Code sending and receiving. This is a 10 minutes duration test to examine the candidate's proficiency in sending and receiving Morse Code.
-          </div>
-          <div class="col-sm" id="text7">
-              Check out our Instagram series, ‘Morse Mania’, where we have posted the graphical representation of morse code for every alphabet and digit which can assist in remembering the morse codes visually.
-         </div>
-     </div> 
-   </div>
-   <div class="container-fluid">
-       <div class="row">
+  </div>
+  <div class="row" id="left">
+      <div class="col-sm-6" id="linear">
+
+      </div>
+      <div class="col-sm-6" id="text8">
+          Check out our Instagram series,<a href="https://www.instagram.com/p/CGurROOng8r/" target="_blank">‘Morse Mania’</a>, where we have posted the graphical representation of morse code for every alphabet and digit, which can assist in remembering the morse codes visually.
+      </div>
+  </div>
+  <div class="row" id="left">
             <div class="col-sm">
                 <img src="images\MorseMania.jpg" class="img1">
             </div>
-          <div class="col-sm" id="text8">
-              <p id="t1">Following is the procedure for the application of Ham Radio License Exam:</p>
+          <div class="col-sm" id="text9">
+              <p id="t1">PROCEDURE FOR THE APPLICATION OF HAM RADIO EXAM:</p>
               <ul id="list">
-                  <li>Application can be downloaded from <a href ="https://saralsanchar.gov.in/" class="link1">here</a>. Exam application along with the fee of Rs. 100 is to be paid online. The examination can be conducted in your own town provided there are a sufficient number of applicants.</li>
-                  <li>On passing the examination, you will be awarded the license on payment of the required license fee: Rs. 1000 for 20 years and Rs. 2000 if you want the license for a lifetime (80 years), on <a href="http://bharatkosh.gov.in/">http://bharatkosh.gov.in/</a></li>
+                  <li>Application can be downloaded from <a href ="https://saralsanchar.gov.in/" target="_blank" class="link1">here</a>. Exam application along with the fee of Rs. 100 is to be paid online. The examination can be conducted in your own town provided there are a sufficient number of applicants.</li>
+                  <li>On passing the examination, you will be awarded the license on payment of the required license fee: Rs. 1000 for 20 years and Rs. 2000 if you want the license for a lifetime (80 years), on <a href="http://bharatkosh.gov.in/" target="_blank">http://bharatkosh.gov.in/</a></li>
               </ul>
          </div>
-     </div> 
-   </div>
-   <div id="text9">
+    </div> 
+    <div id="text10">
       WPC grants the license along with the user-chosen callsign. Indian amateur radio operators are allotted only the VU call-sign prefix. VU call-signs are listed according to license grade: for general license holders, the call-sign prefix is VU2; for restricted license holders, the prefix is VU3.
    </div>
-   <div id="text10">   
+   <div id="text11">   
       Reach out to us for the study material or any other queries regarding the ham license exam.
     </div>
-
 </div>
+
+
+
+
 
 
 
