93abe0e268d58ea99e62dac8b24546b9aec42512
raghavrander <81479687+raghavrander@users.noreply.github.com>
2022-02-03 22:16:50 +0530

    added images, links and fixed text

--- team.php
+++ team.php
@@ -1,11 +1,77 @@
-<?php $title = 'hamexam';
+<?php $title = 'TEAM';
 include 'header.php'; ?>
 
+<head>
 <link rel="stylesheet" href="./css/team.css" />
+<script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+</head>
+
+<div class="container-fluid" style="margin-top:350px;">
 
 
 
-<h1 style="color: white; text-align: center; margin: 50px 0">CURRENT TEAM  MEMBERS</h1>
+<h1 style="color: white; text-align: center; margin: 50px 0">CURRENT TEAM MEMBERS</h1>
+
+<div id="past">
+  <ul class="list-members">
+    <li class="member">
+      <div class="member-image">
+        <img src="images\teams\Arpit_Verma.jpg">
+      </div>
+      <div class="member-info">
+        <h3>Arpit Verma</h3>
+        <p>Founder & CEO</p>
+        <div class="social-link">
+          <a href="https://www.facebook.com/profile.php?id=100009854465275" target="_blank"><i class="fab fa-facebook-f"></i></a>
+          <!-- <i class="fab fa-twitter"></i> -->
+          <a href="https://www.linkedin.com/in/arpit-verma-32b764194/" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+        </div>
+      </div>
+    </li>
+    <li class="member">
+      <div class="member-image">
+        <img src="images\teams\Anuj_Agrawal.jpg">
+      </div>
+      <div class="member-info">
+        <h3>Anuj S Agrawal</h3>
+        <p>Product Manager</p>
+        <div class="social-link">
+          <a href="https://www.facebook.com/profile.php?id=100008234599104" target="_blank"><i class="fab fa-facebook-f"></i></a>
+          <a href="https://twitter.com/anujsagrawal22?s=09" target="_blank"><i class="fab fa-twitter"></i></a>
+          <a href="https://www.linkedin.com/in/anuj-agrawal-a25862194/" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+        </div>
+      </div>
+    </li>
+    <li class="member">
+      <div class="member-image">
+        <img src="images\teams\Pawan_Kumar.jpg">
+      </div>
+      <div class="member-info">
+        <h3>Pawan Mavlia</h3>
+        <p>Marketing Head</p>
+        <div class="social-link">
+          <a href="https://www.facebook.com/profile.php?id=100037822874317" target="_blank"><i class="fab fa-facebook-f"></i></a>
+          <!-- <i class="fab fa-twitter"></i> -->
+          <!-- <a href="https://www.linkedin.com/in/pawan-mavlia-a748171a9" target="_blank"><i class="fab fa-linkedin-in"></i></a> -->
+        </div>
+      </div>
+    </li>
+    <!-- <li class="member">
+      <div class="member-image">
+        <img src="https://fadzrinmadu.github.io/hosted-assets/team-section-design-using-html-and-css/4.png">
+      </div>
+      <div class="member-info">
+        <h3>Jaxon Mathew</h3>
+        <p>Affiliate Manager</p>
+        <div class="social-link">
+          <i class="fab fa-facebook-f"></i>
+          <i class="fab fa-twitter"></i>
+          <i class="fab fa-linkedin-in"></i>
+        </div>
+      </div>
+    </li> -->
+  </ul>
+  <h1 style="color: white; text-align: center;margin:50px 0;">PAST TEAM  MEMBERS</h1>
 
 <!-- <div id="current">
 <ul class="current-list-members">
@@ -72,124 +138,61 @@ include 'header.php'; ?>
 <ul class="list-members">
   <li class="member">
     <div class="member-image">
-      <img src="https://fadzrinmadu.github.io/hosted-assets/team-section-design-using-html-and-css/1.png">
+      <img src="images/teams/dhrumil.jpg">
     </div>
     <div class="member-info">
-      <h3>Hazel Andrea</h3>
+      <h3>Dhrumil Shah</h3>
       <p>Founder & CEO</p>
       <div class="social-link">
-        <i class="fab fa-facebook-f"></i>
-        <i class="fab fa-twitter"></i>
-        <i class="fab fa-linkedin-in"></i>
+        <a href="https://www.facebook.com/dhrumil.shah.1420" target="_blank"><i class="fab fa-facebook-f"></i></a>
+        <!-- <i class="fab fa-twitter"></i> -->
+        <a href="https://www.linkedin.com/in/dhrumil303" target="_blank"><i class="fab fa-linkedin-in"></i></a>
       </div>
     </div>
   </li>
   <li class="member">
     <div class="member-image">
-      <img src="https://fadzrinmadu.github.io/hosted-assets/team-section-design-using-html-and-css/2.png">
+      <img src="images/teams/ritik.jpg">
     </div>
     <div class="member-info">
-      <h3>Lucas Edward</h3>
+      <h3>Ritik Bilala</h3>
       <p>Product Manager</p>
       <div class="social-link">
-        <i class="fab fa-facebook-f"></i>
-        <i class="fab fa-twitter"></i>
-        <i class="fab fa-linkedin-in"></i>
+        <a href="https://www.facebook.com/RitikBilala" target="_blank"><i class="fab fa-facebook-f"></i></a>
+        <!-- <i class="fab fa-twitter"></i> -->
+        <a href="https://www.linkedin.com/in/ritikbilala" target="_blank"><i class="fab fa-linkedin-in"></i></a>
       </div>
     </div>
   </li>
   <li class="member">
     <div class="member-image">
-      <img src="https://fadzrinmadu.github.io/hosted-assets/team-section-design-using-html-and-css/3.png">
+      <img src="images/teams/siddhi.jpg">
     </div>
     <div class="member-info">
-      <h3>Ella Piper</h3>
+      <h3>Siddhi Nagre</h3>
       <p>Marketing Head</p>
       <div class="social-link">
-        <i class="fab fa-facebook-f"></i>
-        <i class="fab fa-twitter"></i>
-        <i class="fab fa-linkedin-in"></i>
+        <a href="https://www.facebook.com/siddhi.nagre" target="_blank"><i class="fab fa-facebook-f"></i></a>
+        <!-- <i class="fab fa-twitter"></i> -->
+        <a href="https://www.linkedin.com/in/siddhi-nagre-b71157173" target="_blank"><i class="fab fa-linkedin-in"></i></a>
       </div>
     </div>
   </li>
   <li class="member">
     <div class="member-image">
-      <img src="https://fadzrinmadu.github.io/hosted-assets/team-section-design-using-html-and-css/4.png">
+      <img src="images\teams\Arpit_Verma.jpg">
     </div>
     <div class="member-info">
-      <h3>Jaxon Mathew</h3>
+      <h3>Arpit Verma</h3>
       <p>Affiliate Manager</p>
       <div class="social-link">
-        <i class="fab fa-facebook-f"></i>
-        <i class="fab fa-twitter"></i>
-        <i class="fab fa-linkedin-in"></i>
+      <a href="https://www.facebook.com/profile.php?id=100009854465275" target="_blank"><i class="fab fa-facebook-f"></i></a>
+          <!-- <i class="fab fa-twitter"></i> -->
+          <a href="https://www.linkedin.com/in/arpit-verma-32b764194/" target="_blank"><i class="fab fa-linkedin-in"></i></a>
       </div>
     </div>
   </li>
 </ul>
 </div>
 
-
-
-<h1 style="color: white; text-align: center; margin: 50px 0">PAST TEAM MEMBERS</h1>
-
-<div id="past">
-  <ul class="list-members">
-    <li class="member">
-      <div class="member-image">
-        <img src="https://fadzrinmadu.github.io/hosted-assets/team-section-design-using-html-and-css/1.png">
-      </div>
-      <div class="member-info">
-        <h3>Hazel Andrea</h3>
-        <p>Founder & CEO</p>
-        <div class="social-link">
-          <i class="fab fa-facebook-f"></i>
-          <i class="fab fa-twitter"></i>
-          <i class="fab fa-linkedin-in"></i>
-        </div>
-      </div>
-    </li>
-    <li class="member">
-      <div class="member-image">
-        <img src="https://fadzrinmadu.github.io/hosted-assets/team-section-design-using-html-and-css/2.png">
-      </div>
-      <div class="member-info">
-        <h3>Lucas Edward</h3>
-        <p>Product Manager</p>
-        <div class="social-link">
-          <i class="fab fa-facebook-f"></i>
-          <i class="fab fa-twitter"></i>
-          <i class="fab fa-linkedin-in"></i>
-        </div>
-      </div>
-    </li>
-    <li class="member">
-      <div class="member-image">
-        <img src="https://fadzrinmadu.github.io/hosted-assets/team-section-design-using-html-and-css/3.png">
-      </div>
-      <div class="member-info">
-        <h3>Ella Piper</h3>
-        <p>Marketing Head</p>
-        <div class="social-link">
-          <i class="fab fa-facebook-f"></i>
-          <i class="fab fa-twitter"></i>
-          <i class="fab fa-linkedin-in"></i>
-        </div>
-      </div>
-    </li>
-    <!-- <li class="member">
-      <div class="member-image">
-        <img src="https://fadzrinmadu.github.io/hosted-assets/team-section-design-using-html-and-css/4.png">
-      </div>
-      <div class="member-info">
-        <h3>Jaxon Mathew</h3>
-        <p>Affiliate Manager</p>
-        <div class="social-link">
-          <i class="fab fa-facebook-f"></i>
-          <i class="fab fa-twitter"></i>
-          <i class="fab fa-linkedin-in"></i>
-        </div>
-      </div>
-    </li> -->
-  </ul>
 </div>
