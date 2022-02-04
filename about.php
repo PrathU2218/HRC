c8542522726fc6947b59e57a56a7c6283a1245e2
raghavrander <81479687+raghavrander@users.noreply.github.com>
2022-02-04 18:39:11 +0530

    fixed text and responsive

--- about.php
+++ about.php
@@ -10,7 +10,7 @@
 </head>
 
 <body>
-    <div class="container-fluid" id="i01">
+    <div class="container-fluid i01">
     <div style="min-height:350px;"></div>
         <div id="i02left">
             <div>
@@ -30,8 +30,7 @@
                 <div id="i04">
                     Ham is a hobby that can be pursued by anyone who owns a transceiver like handheld radio or Baofeng (more commonly known as a walkie-talkie). A ham radio operator can talk to other ham radio operators across the world and even receive signals which can be deciphered later using a simple application to convert them into images or voice or morse code. These signals could come from either space or from far-off parts of the world. Ham Radio has captivated the interest of countless thousands and provides a stimulating technical hobby for people around the globe. Many ham operators enjoy talking to far and distant friends. We wish to familiarize people with the basics of what ham is and promote the amateur radio culture through various events, talks, and hands-on sessions.
                     <br />
-                    Read more about the very basics of Ham Radio in our blog: Ham Radio 101
-                    <a href="https://hrciitb.wordpress.com/2020/08/24/ham-radio-101/">https://hrciitb.wordpress.com/2020/08/24/ham-radio-101/</a>
+                    Read more about the very basics of Ham Radio in our blog: <a href="https://hrciitb.wordpress.com/2020/08/24/ham-radio-101/">Ham Radio 101</a>
                 </div>
             </div>
         </div>
@@ -44,8 +43,7 @@
                 <div id="i05">
                     Peek into our radio shack and find out the basic details of all the hardware and equipment we use in
                     order to communicate and track satellites.
-                    This was also covered in our Instagram series “Know our Shack”:
-                    <a href="https://www.instagram.com/p/CFUXaHHHSG5/">https://www.instagram.com/p/CFUXaHHHSG5/</a>
+                    This was also covered in our Instagram series <a href="https://www.instagram.com/p/CFUXaHHHSG5/">“Know our Shack”</a>
 
                 </div>
             </div>
@@ -56,17 +54,17 @@
             <div class="row">
                 <!-- <div class="flex-container"> -->
                 <!-- <div> -->
-                <div class="column">
+                <div class="col-sm-6" id="contentcol">
                     <div id="i04below">
                         Yagi-Uda Antenna
                         <br>
-                        The Yagi-Uda antenna is one of the most used antenna designs. It typically operates in the HF and UHF bands (about 3 MHz to 3 GHz). It is a highly directive antenna and is generally used for TV reception, tracking the International Space Station, and weather satellites such as NOAA 15, 18, and 19
+                        The Yagi-Uda antenna is one of the most used antenna designs. It typically operates in the HF and UHF bands (about 3 MHz to 3 GHz). It is a highly directive antenna and is generally used for TV reception, tracking the International Space Station, and weather satellites such as NOAA 15, 18, and 19.
 
                     </div>
                 </div>
                 <!-- </div> -->
                 <!-- <div> -->
-                <div class="column">
+                <div class="col-sm-6">
                     <div>
                         <img src="./images/yagi.jpg" id="i06" class="image-left"></img>
                     </div>
@@ -79,16 +77,16 @@
 
         <div class="container" id="i02below">
             <div class="row">
-            <!-- <div class="flex-container"> -->
+               <!-- <div class="flex-container"> -->
                 <!-- <div> -->
-                <div class="column">
+                <div class="col-sm-6">
                     <div>
                         <img src="./images/hand.jpeg" id="i06"></img>
                     </div>
                 </div>
                 <!-- </div> -->
                 <!-- <div> -->
-                <div class="column">
+                <div class="col-sm-6" id="contentcol">
                     <div id="i04below">
                         Hand-held Radio
                         <br>
@@ -97,7 +95,7 @@
                     </div>
                 </div>
                 <!-- </div> -->
-                </div>
+                
             </div>
         </div>
 
@@ -105,7 +103,7 @@
             <div class="row">
             <!-- <div class="flex-container"> -->
                 <!-- <div> -->
-                <div class="column">
+                <div class="col-sm-6" id="contentcol">
                     <div id="i04below">
                         Base Station Transceiver
                         <br>
@@ -115,13 +113,13 @@
                 </div>
                 <!-- </div> -->
                 <!-- <div> -->
-                <div class="column">
+                <div class="col-sm-6">
                     <div>
                         <img src="./images/base.jpeg" id="i06" class="image-left"></img>
                     </div>
                 </div>
                 <!-- </div> -->
-                </div>
+                
             </div>
         </div>
 
@@ -129,14 +127,14 @@
             <div class="row">
             <!-- <div class="flex-container"> -->
                 <!-- <div> -->
-                <div class="column">
+                <div class="col-sm-6">
                     <div>
                         <img src="./images/double.jpg" id="i06"></img>
                     </div>
                 </div>
                 <!-- </div> -->
                 <!-- <div> -->
-                <div class="column">
+                <div class="col-sm-6" id="contentcol">
                     <div id="i04below">
                     Double-Cross Antenna
                         <br>
@@ -144,7 +142,7 @@
                     </div>
                 </div>
                 <!-- </div> -->
-                </div>
+                
             </div>
         </div>
 
@@ -152,7 +150,7 @@
             <div class="row">
             <!-- <div class="flex-container"> -->
                 <!-- <div> -->
-                <div class="column">
+                <div class="col-sm-6" id="contentcol">
                     <div id="i04below">
                     Amplifiers
 
@@ -164,64 +162,78 @@
                 </div>
                 <!-- </div> -->
                 <!-- <div> -->
-                <div class="column">
+                <div class="col-sm-6">
                     <div>
                         <img src="./images/amplifier.jpg" id="i06" class="image-left"></img>
                     </div>
                 </div>
                 <!-- </div> -->
-                </div>
+         
             </div>
         </div>
-
+     
         <div class="container" id="i02below">
             <div class="row">
-            <div class="flex-container">
-                <div>
-                <div class="column">
-                    <div>
-                        <img src="./images/cable.jpg" id="i06"></img>
-                    </div>
-                </div>
-                </div>
-                <div>
-                <div class="column">
+            <!-- <div class="flex-container"> -->
+                <!-- <div> -->
+                <!-- <div class="column">
                     <div id="i04below">
                     Cables and Connectors
 
                         <br>
                         Cables and Connectors, as the name suggests, are required to connect two or more devices or equipment in our ground station. For RF transmission, the coaxial cable makes sure not to let the electromagnetic waves distort and helps signals travel larger distances. Each cable has its specific connector, i.e., coaxial connectors are used with coaxial cable. The two types of connectors- male connectors and female connectors depend on the location of threading and polarity. 
- 
+
+
+                    </div>
+                </div> -->
+                <!-- </div> -->
+                <!-- <div> -->
+                <div class="col-sm-6">
+                    <div>
+                        <img src="images/cable.jpg" id="i06"></img>
+                    </div>
+                </div>
+                <div class="col-sm-6" id="contentcol">
+                    <div id="i04below">
+                    Cables and Connectors
+
+                        <br>
+                        Cables and Connectors, as the name suggests, are required to connect two or more devices or equipment in our ground station. For RF transmission, the coaxial cable makes sure not to let the electromagnetic waves distort and helps signals travel larger distances. Each cable has its specific connector, i.e., coaxial connectors are used with coaxial cable. The two types of connectors- male connectors and female connectors depend on the location of threading and polarity. 
+
 
                     </div>
                 </div>
-                </div>
+                <!-- </div> -->
+         
             </div>
-            </div>
-        </div>
+        </div>   
 
         <div class="container" id="i02below">
             <div class="row">
-            <div class="flex-container">
-                <div>
-                <div class="column">
+            <!-- <div class="flex-container"> -->
+                <!-- <div> -->
+                <div class="col-sm-6" id="contentcol">
                     <div id="i04below">
                     Antenna Rotator
+
                         <br>
                         An antenna rotator is a device used to change the orientation of a directional antenna. It has two parts- a rotor which is used to move the antenna to point in the direction of the satellite, and a controller for the rotor to make the process of tracking autonomous. The controller is normally placed near the antenna while the rotator is mounted on the antenna mast.
+
+
+
                     </div>
                 </div>
-                </div>
-                <div>
-                <div class="column">
+                <!-- </div> -->
+                <!-- <div> -->
+                <div class="col-sm-6">
                     <div>
-                        <img src="./images/rotator.jpg" id="i06" class="image-left"></img>
+                        <img src="images/rotator.jpg" id="i06" class="image-left"></img>
                     </div>
                 </div>
+                <!-- </div> -->
+         
             </div>
-                </div>
-                </div>
-        </div>
+        </div> 
 
     </div>
 </body>
