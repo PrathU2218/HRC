854328f9f19fd2ddab1cb983fbdc309bc54e3229
Prathmesh Namdeo <31315757+PrathU2218@users.noreply.github.com>
2022-02-06 01:34:55 +0530

    corrected font awesome cdn

--- footer.php
+++ footer.php
@@ -1,5 +1,7 @@
+<!-- <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
+<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
 
-<script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
 
 <style>
   @import url(https://fonts.googleapis.com/css?family=Open+Sans:400,500,300,700);
